804e575454cf5ca2c76eeee733587a04e8087198
Mirzabek <mirzabekmirzayev746@gmail.com>
2023-03-02 19:28:55 +0500

    Initial Commit

---
+++ MovieBookingApp/MovieBookingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieBookingAppApp.swift
+//  MovieBookingApp
+//
+//  Created by Mirzabek on 02/03/23.
+//
+
+import SwiftUI
+
+@main
+struct MovieBookingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
